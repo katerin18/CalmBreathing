c26fe669fc47a796db302561f843fae083da1cd6
GeFy13 <egor.fiyalo@gmail.com>
2026-04-10 14:19:19 +0300

    Init project (Spring Boot, Liquibase, PSQL)

---
+++ backend/demo/src/test/java/mpi/calmbreath/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mpi.calmbreath.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
